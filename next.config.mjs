6c33286b35cf19157f0b2b79dcb963b16b3d9475
prakashjung-devloper <jungprakash426@gmail.com>
2026-08-23 17:08:48 +0545

    Fix GitHub Pages path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
   output: "export",
+
+  basePath: "/My-Portfolio-website",
+
   images: {
     unoptimized: true,
   },
